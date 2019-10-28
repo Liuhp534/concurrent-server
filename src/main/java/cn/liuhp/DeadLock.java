baa643c4c0f1787546c69f83ed20e9e641644cde
刘辉鹏 <liuhuipeng@huize.com>
2019-10-28 08:42:17 +0800

    ReentrantLock实现死锁

--- src/main/java/cn/liuhp/DeadLock.java
+++ src/main/java/cn/liuhp/DeadLock.java
@@ -1,5 +1,7 @@
 package cn.liuhp;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @description: 死锁例子
  * @author: hz16092620
@@ -7,19 +9,55 @@ package cn.liuhp;
  */
 public class DeadLock {
 
+
+    public static void main(String[] args) {
+        //test1();
+        //test2();
+        test3();
+    }
+
+    /*
+     * 死锁的例子
+     * */
+    private static void test1() {
+        DeadLock deadLock = new DeadLock();
+        new Thread(() -> deadLock.m1()).start();
+        new Thread(() -> deadLock.m2()).start();
+    }
+
+    /*
+     * volatile的使用
+     * */
+    private static void test2() {
+        DeadLock deadLock = new DeadLock();
+        new Thread(() -> deadLock.m2()).start();
+        SleepUtils.sleepSeconds(3);
+        System.out.println("m2 退出执行逻辑");
+        deadLock.flag = Boolean.FALSE;//执行重排序
+    }
+
+    /*
+     * 死锁的例子通过ReentrantLock
+     * */
+    private static void test3() {
+        ReentrantLockDeadLockTest reentrantLockDeadLockTest1 = new DeadLock.ReentrantLockDeadLockTest("lockone");
+        ReentrantLockDeadLockTest reentrantLockDeadLockTest2 = new DeadLock.ReentrantLockDeadLockTest("locktwo");
+        new Thread(() -> reentrantLockDeadLockTest1.m3()).start();
+        new Thread(() -> reentrantLockDeadLockTest1.m4()).start();
+    }
+
     volatile boolean flag = Boolean.TRUE;
     Object lock1 = new Object();
     Object lock2 = new Object();
 
     /**
-    * @Description:
-     * m1完成任务需要lock1和lock2，先获取lock1，再去获取lock2
-    * @param
-    * @return
-    * @throws
-    * @author hz16092620
-    * @date 2019/3/25 15:56
-    */
+     * @param
+     * @return
+     * @throws
+     * @Description: m1完成任务需要lock1和lock2，先获取lock1，再去获取lock2
+     * @author hz16092620
+     * @date 2019/3/25 15:56
+     */
     public void m1() {
         synchronized (lock1) {//获取lock1锁
             System.out.println("m1 使用lock1");
@@ -32,14 +70,13 @@ public class DeadLock {
     }
 
     /**
-    * @Description:
-     * m2 一直使用说lock2，m1一直拿不到
-    * @param
-    * @return
-    * @throws
-    * @author hz16092620
-    * @date 2019/3/25 15:55
-    */
+     * @param
+     * @return
+     * @throws
+     * @Description: m2 一直使用说lock2，m1一直拿不到
+     * @author hz16092620
+     * @date 2019/3/25 15:55
+     */
     public void m2() {
         synchronized (lock2) {
             System.out.println("m2 使用lock2");
@@ -54,27 +91,62 @@ public class DeadLock {
         }
     }
 
-    public static void main(String[] args) {
-        test1();
-    }
 
     /*
-    * 死锁的例子
-    * */
-    private static void test1() {
-        DeadLock deadLock = new DeadLock();
-        new Thread(() -> deadLock.m1()).start();
-        new Thread(() -> deadLock.m2()).start();
+     * 静态内部类
+     * */
+    private static class ReentrantLockDeadLockTest {
+        /*
+         * ===============通过ReentranLock
+         * */
+        private String name;
+
+        private static ReentrantLock reentrantLock1 = new ReentrantLock();
+
+        private static ReentrantLock reentrantLock2 = new ReentrantLock();
+
+        public ReentrantLockDeadLockTest(String name) {
+            this.name = name;
+        }
+
+        /*
+         * 测试ReentranLock
+         * */
+        public void m3() {
+            try {
+                reentrantLock1.lock();
+                System.out.println(this.name + ".m3 使用reentrantLock1");
+                SleepUtils.sleepSeconds(1);
+                try {
+                    reentrantLock2.lock();
+                    System.out.println(this.name + ".m3 使用reentrantLock2");
+                } finally {
+                    reentrantLock2.unlock();
+                }
+            } finally {
+                reentrantLock1.unlock();//一定要释放掉
+            }
+        }
+
+        /*
+         * 测试ReentranLock
+         * */
+        public void m4() {
+            try {
+                reentrantLock2.lock();
+                System.out.println(this.name + ".m4 使用reentrantLock2");
+                SleepUtils.sleepSeconds(1);
+                try {
+                    reentrantLock1.lock();
+                    System.out.println(this.name + ".m4 使用reentrantLock1");
+                } finally {
+                    reentrantLock1.unlock();
+                }
+            } finally {
+                reentrantLock2.unlock();//一定要释放掉
+            }
+        }
     }
 
-    /*
-    * volatile的使用
-    * */
-    private static void test2() {
-        DeadLock deadLock = new DeadLock();
-        new Thread(() -> deadLock.m2()).start();
-        SleepUtils.sleepSeconds(3);
-        System.out.println("m2 退出执行逻辑");
-        deadLock.flag = Boolean.FALSE;//执行重排序
-    }
+
 }
