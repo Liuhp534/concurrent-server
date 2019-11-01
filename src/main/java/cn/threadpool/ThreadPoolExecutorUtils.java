6eab90d0e94b70c2fdd02bebbf0fa83fca039833
刘辉鹏 <liuhuipeng@huize.com>
2019-11-01 15:19:51 +0800

    线程池工具

---
+++ src/main/java/cn/threadpool/ThreadPoolExecutorUtils.java
@@ -0,0 +1,20 @@
+package cn.threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/*
+*
+* 线程池工具
+* */
+public class ThreadPoolExecutorUtils {
+
+
+    /*
+    * 线程池
+    * */
+    public static final ExecutorService executorService = new ThreadPoolExecutor(5, 10, 10L, TimeUnit.SECONDS,
+            new LinkedBlockingQueue<Runnable>());//核心线程数2，最大线程数4，线程多久空闲销毁，工作队列
+}
