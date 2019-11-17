aaf903eddd766038536bb53a1c25431831ec439e
Liuhp534 <617456341@qq.com>
2019-11-17 11:23:28 +0800

    对于异常的优化

--- src/main/java/cn/biglist/DealBigList.java
+++ src/main/java/cn/biglist/DealBigList.java
@@ -7,61 +7,96 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
+/*
+* , new ThreadPoolExecutor.DiscardPolicy() {
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            System.out.println("被拒接的任务");
+            if (!executor.isShutdown()) {//改变future的状态，让其不会一直阻塞get
+                if (r != null && r instanceof FutureTask) {
+                    ((FutureTask) r).cancel(true);
+                }
+            }
+        }
+    }
+* */
 public class DealBigList {
 
     //4-耗时=4587
     //10-耗时=1998
     //50-耗时=417
     //100-耗时=220
-    static ExecutorService executorService = new ThreadPoolExecutor(200, 200, 60L, TimeUnit.SECONDS,
-            new LinkedBlockingQueue<Runnable>());//核心线程数2，最大线程数4，线程多久空闲销毁，工作队列
+    static ExecutorService executorService = new ThreadPoolExecutor(1, 1, 60L, TimeUnit.SECONDS,
+            new LinkedBlockingQueue<Runnable>(1));//核心线程数2，最大线程数4，线程多久空闲销毁，工作队列
 
 
     public static void main(String[] args) {
         System.out.println(Runtime.getRuntime().availableProcessors());
         long start = System.currentTimeMillis();
-        fun1();
+        try {
+            fun1();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("fun1 异常");
+        }
         System.out.println("耗时=" + (System.currentTimeMillis() - start));
     }
 
-
+    /*
+     * 如果1和2两个步骤的异常，没有捕获，那么就会导致3不会执行完全
+     * 好吧3try/catch也不能执行完，原来是catch的异常没有捕获到，导致的，直接Exception就可以捕获所有的
+     * */
     private static void fun1() {
-        List<Future<Integer>> dealResult = new ArrayList<>();
+
         int pageSize = 100;
-        int totalRecord = 100000;
+        int totalRecord = 999;
         int pageCount = (totalRecord  +  pageSize  - 1) / pageSize;
         List<Integer> bigList = new ArrayList<>(totalRecord);
         initBigList(bigList, totalRecord);
         int start = 0;
         int end = 0;
-        for (int i = 1; i <= pageCount; i ++) {
-            start = (i - 1) * pageSize;
-            end = i * pageSize;
-            /*if (end > totalRecord) {
-                end = totalRecord - 1;
-            }*/
-            DealBigListHandler handler = new DealBigListHandler(bigList.subList(start, end));
-            try {
-                dealResult.add(executorService.submit(handler));
-            } catch (Exception e) {
-                System.out.println("异常");
+        //1、分配任务，这一步异常则不会执行任何任务
+        System.out.println("------------------执行第1步------------------");
+        List<DealBigListHandler> taskHandlerList = new ArrayList<>(pageCount);
+        DealBigListHandler taskHandler = null;
+        try {
+            for (int i = 1; i <= pageCount; i++) {
+                start = (i - 1) * pageSize;
+                end = i * pageSize;
+                if (end > totalRecord) {
+                    end = totalRecord;
+                }
+                taskHandler = new DealBigListHandler(bigList.subList(start, end));
+                taskHandlerList.add(taskHandler);
             }
-
+        } catch (Exception e) {
+            System.out.println("创建任务异常");
+            return;
         }
-
-        //打印执行情况
+        //2、提交任务，如果不捕获or没有捕获到，那么方法不放下执行。这一步异常则会执行已提交的任务
+        System.out.println("------------------执行第2步------------------");
+        List<Future<Integer>> dealResult = new ArrayList<>();
+        try {
+            for (DealBigListHandler dealBigListHandler : taskHandlerList) {
+                dealResult.add(executorService.submit(dealBigListHandler));
+            }
+        } catch (Exception e) {
+            System.out.println("提交任务异常，提交数量=" + dealResult.size());
+        }
+        //3、提取执行情况，如果不捕获or没有捕获到，那么方法不放下执行。这一步异常则不能正确获取执行的结果，但是不影响执行
+        System.out.println("------------------执行第3步------------------");
         int result = 0;
         for (Future<Integer> integerFuture : dealResult) {
             try {
                 result += integerFuture.get();
-                System.out.println("result : " + result);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } catch (ExecutionException e) {
                 e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
-        System.out.println(result);
+        System.out.println("完成情况=" + result);
         /*try {
             int result = 0;
             for (Future<Integer> integerFuture : dealResult) {
