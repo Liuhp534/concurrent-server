c2ea85eef3040f2da48738528a319b5cedba8a5b
Liuhp534 <617456341@qq.com>
2020-02-15 22:32:52 +0800

    现日志打印问题，是因为获取打印的日志没有问题，而是，释放锁打印日志的问题，在加锁和释放锁之间就不会有问题，打印是之后，所以就。

--- src/main/java/cn/lock/SpinLock.java
+++ src/main/java/cn/lock/SpinLock.java
@@ -2,6 +2,7 @@ package cn.lock;
 
 import cn.liuhp.SleepUtils;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -11,19 +12,34 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 public class SpinLock {
 
+    int count = 0;
 
-    public static void main(String[] args) {
-        SpinLock spinLock = new SpinLock();
-        for (int i = 0; i < 5; i ++) {
-            new Thread(() -> {
-                //spinLock.lock();
-                //SleepUtils.sleepMillis(50);
-                spinLock.unLock();
-            }, "t" + i).start();
-        }
+    public void addCount() {
+        //System.out.println(Thread.currentThread().getName() + "\t递增");
+        count ++;
     }
 
-    private static final AtomicReference<Boolean> atomicR = new AtomicReference<>(Boolean.FALSE);
+    /*
+    * count是正确的，也就是说，加锁成功了。但是打印加锁的信息会显示同时两个线程加了锁。
+    * */
+    public static void main(String[] args) throws InterruptedException {
+        int threadSize = 1000;
+        CountDownLatch countDownLatch = new CountDownLatch(threadSize);
+        SpinLock spinLock = new SpinLock();
+        for (int i = 0; i < threadSize; i ++) {
+            new Thread(() -> {
+                spinLock.lock();
+                spinLock.addCount();
+                //SleepUtils.sleepMillis(50);
+                spinLock.unLock();
+                countDownLatch.countDown();
+            }, "t" + i).start();
+        }
+        countDownLatch.await();
+        System.out.println(spinLock.count);
+    }
+
+    private static  volatile AtomicReference<Boolean> atomicR = new AtomicReference<>(Boolean.FALSE);
 
     private AtomicReference<Thread> atomicT = new AtomicReference<>(null);
 
@@ -32,26 +48,26 @@ public class SpinLock {
         Thread thread = Thread.currentThread();
         while (!atomicT.compareAndSet(null, thread)) {
             //System.out.println("未获取到锁");
+            //SleepUtils.sleepMillis(10);
         }
+        System.out.println("获取到锁");
         /*while (!atomicR.compareAndSet(Boolean.FALSE, Boolean.TRUE)) {
             //当while一定时间，返回false不继续
         }*/
-        System.out.println(Thread.currentThread().getName() + "\t获取到锁" + "\t 获取锁的线程=" + atomicT.get().getName());
-        if (!Thread.currentThread().getName().equals(atomicT.get().getName())) {
-            System.out.println(Thread.currentThread().getName() + "\t获取到锁" + "\t 获取锁的线程=" + atomicT.get().getName());
-            System.out.println("不一致情况");
-        }
+        //System.out.println(Thread.currentThread().getName() + "\t获取到锁" + "\t 获取锁的线程=" + atomicT.get().getName());
+
     }
 
     public void unLock() {
         Thread thread = Thread.currentThread();
+        System.out.println("释放锁");
         while (!atomicT.compareAndSet(thread, null)) {
             System.out.println("释放锁失败");
         }
         /*while (!atomicR.compareAndSet(Boolean.TRUE, Boolean.FALSE)) {
 
         }*/
-        System.out.println(Thread.currentThread().getName() + "\t释放锁");
+        //System.out.println(Thread.currentThread().getName() + "\t释放锁");
     }
 
 
